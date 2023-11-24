0366ab275a585c03297765bb366b885cd88a23a8
Whatwhy <hdk8572@gmail.com>
2023-11-24 15:07:03 +0900

    로그아웃 기능 구현 완료

--- SpringMVC03/src/main/java/kr/board/controller/MemberController.java
+++ SpringMVC03/src/main/java/kr/board/controller/MemberController.java
@@ -68,4 +68,39 @@ public class MemberController {
 			return "redirect:/memJoin.do";
 			}
 	}
+	
+	//	로그아웃 처리
+	@RequestMapping("/memLogout.do")
+	public String memLogout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
+	}	
+	
+	//	로그인 화면으로 이동
+	@RequestMapping("/memLoginForm.do")
+	public String memLoginForm() {
+		return "member/memLoginForm"; //	memLoginForm.jsp
+	}
+	
+	//	로그인 기능
+	@RequestMapping("/memLogin.do")
+	public String memLogin(Member m, RedirectAttributes rttr, HttpSession session) {
+		if(m.getMemID()==null || m.getMemID().equals("") ||
+		   m.getMemPassword()==null || m.getMemPassword().equals("")) {
+			rttr.addFlashAttribute("msgType","실패 메세지");
+			rttr.addFlashAttribute("msg","모든 내용을 입력해주세요.");
+			return "redirect:/memLoginForm.do";
+		}
+		Member mvo = memberMapper.memLogin(m);
+		if(mvo!=null) {	// 로그인에 성공
+			rttr.addFlashAttribute("msgType", "성공 메세지");
+			rttr.addFlashAttribute("msg", "로그인에 성공했습니다.");
+			session.setAttribute("mvo", mvo);
+			return "redirect:/";		// 메인
+		} else {		// 로그인에 실패
+			rttr.addFlashAttribute("msgType", "실패 메세지");
+			rttr.addFlashAttribute("msg", "다시 로그인 해주세요.");
+			return "redirect:/memLoginForm.do";		// 메인
+		}
+	}
 }
--- SpringMVC03/src/main/java/kr/board/mapper/MemberMapper.java
+++ SpringMVC03/src/main/java/kr/board/mapper/MemberMapper.java
@@ -8,5 +8,6 @@ import kr.board.entity.Member;
 @Mapper
 public interface MemberMapper {
 	public Member registerCheck(String memID);
-	public int register(Member m); // 회원등록(1, 0)
+	public int register(Member m);		// 회원등록(1, 0)
+	public Member memLogin(Member mvo);	// 로그인 체크
 }
